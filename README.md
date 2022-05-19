b1b40a7d4792c01e6044ac45c90df5a74238a1cb
Michael Saul <michael.saul@gmail.com>
2022-05-19 12:53:07 +0200

    Updated for 2022 Updated to remove depreciated classes. Updated to search different counters.  Please enter the commit message for your changes. Lines starting

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-# bot-rdv-prefecture
-bot pour trouver rendez-vous disponible en préfecture.
+# Prefecture Bot
 
-Fonctionnait début 2019.
-A remettre à jour.
+## Francais
+
+Un bot pour trouver rendez-vous disponible à la préfecture.
+
+Fonctionnait à Paris en Mai 2022.
+
+À utiliser à vos risques et périls.
+
+## English
+
+A bot to find an available meeting at the prefecutre.
+
+Worked in Paris in May 2022.
+
+Use at your own risk.
+
+## Requirements
+- selenium
+- webdriver-manager
--- prefecture.py
+++ prefecture.py
@@ -1,14 +1,24 @@
 import time
 import datetime
+import random
 
 from selenium import webdriver
+from selenium.webdriver.common import by
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 import smtplib, ssl
 
-driver = webdriver.Chrome('D:\chromedriver.exe')
-driver.maximize_window()
+url = "https://pprdv.interieur.gouv.fr/booking/create/989"
+planning_num = ["990" , "1000", "1001", "1002", "1003", "1005", "1028", "1029"]
+index = list(range(0,8))
 
+#driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#driver.maximize_window()
+
+sendemail = False
 port = 587  # For starttls
 smtp_server = "smtp.live.com"
 sender_email = "email@hotmail.com"
@@ -19,43 +29,65 @@ Subject: Check PREFECTURE
 This message is sent from Python."""
 
 while True:
-	driver.get("http://www.essonne.gouv.fr/booking/create/13185/1")
+	driver.get(url)
 
-	time.sleep(10)
+	time.sleep(3)
 
 	bad_gateway = 0
 	indispo = 0
+	overloaded = 0
 
 	try:
-		bad_gateway = driver.find_element_by_xpath("//*[contains(text(), 'Bad Gateway')]")
+		bad_gateway = driver.find_element(by.By.XPATH,"//*[contains(text(), 'Bad Gateway')]")
 	except NoSuchElementException:
 		True
 
 	try:
-		elem = driver.find_element_by_id('planning14167').click()
+		elem = driver.find_element(by.By.ID,"condition").click()
 	except NoSuchElementException:
 		True
 
 	time.sleep(1)
 
 	try:
-		elem = driver.find_element_by_name('nextButton').click()
+		elem = driver.find_element(by.By.NAME,"nextButton").click()
+	except NoSuchElementException:
+		True
+
+	time.sleep(5)
+
+	try:
+		ran = random.choice(index)
+		planning_click='planning'+planning_num[ran]
+		elem = driver.find_element(by.By.ID,planning_click).click()
+	except NoSuchElementException:
+		True
+
+	time.sleep(1)
+
+	try:
+		elem = driver.find_element(by.By.NAME,"nextButton").click()
 	except NoSuchElementException:
 		True
 
 	time.sleep(10)
 
 	try:
-		indispo = driver.find_element_by_xpath("//*[contains(text(), 'existe plus de plage horaire')]")
+		indispo = driver.find_element(by.By.XPATH,"//*[contains(text(),'existe plus de plage horaire')]")
 	except NoSuchElementException:
 		True
 
 	try:
-		bad_gateway = driver.find_element_by_xpath("//*[contains(text(), 'Bad Gateway')]")
+		bad_gateway = driver.find_element(by.By.XPATH,"//*[contains(text(), 'Bad Gateway')]")
 	except NoSuchElementException:
 		True
 
-	f = open('D:\prefecture.txt', 'a+')
+	try:
+		overloaded = driver.find_element(by.By.XPATH,"//*[contains(text(), 'Service surchargé')]")
+	except NoSuchElementException:
+		True
+
+	f = open('prefecture.txt', 'a+')
 	ts = time.time()
 	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 	if indispo != 0:
@@ -64,16 +96,20 @@ while True:
 	if bad_gateway != 0:
 		print("Bad Gateway")
 		f.write(st + " : Bad Gateway" + "\n")
-	if indispo == 0 and bad_gateway == 0:
+	if overloaded != 0:
+		print("Service Overloaded")
+		f.write(st +" :Service Overloaded" + "\n")
+	if indispo == 0 and bad_gateway == 0 and overloaded == 0:
 		print("OK !!")
 		f.write(st + " : OK !!" + "\n")
-		context = ssl.create_default_context()
-		with smtplib.SMTP(smtp_server, port) as server:
-			server.ehlo()  # Can be omitted
-			server.starttls(context=context)
-			server.ehlo()  # Can be omitted
-			server.login(sender_email, password)
-			server.sendmail(sender_email, "receiver1@laposte.net", message)
-			server.sendmail(sender_email, "receiver2@googlemail.com", message)
+		if sendemail:
+			context = ssl.create_default_context()
+			with smtplib.SMTP(smtp_server, port) as server:
+				server.ehlo()  # Can be omitted
+				server.starttls(context=context)
+				server.ehlo()  # Can be omitted
+				server.login(sender_email, password)
+				server.sendmail(sender_email, "receiver1@laposte.net", message)
+				server.sendmail(sender_email, "receiver2@googlemail.com", message)
 	f.close()
 	time.sleep(60)
